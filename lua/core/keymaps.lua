1eb51886ef0f3c24194837501e22289392173133
daan <contact@daanblom.com>
2025-07-25 17:19:44 +0200

    Adding descriptions for which-key

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,42 +6,42 @@ vim.g.maplocalleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opt) -- Move selected lines down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opt) -- Move selected lines up
 
-vim.keymap.set("n", "J", "mzJ`z", opt) -- Keep cursor in the middle when joining lines
+vim.keymap.set("n", "J", "mzJ`z", opt)            -- Keep cursor in the middle when joining lines
 
-vim.keymap.set("n", "n", "nzzzv", opt) -- Keep cursor in the middle when searching
-vim.keymap.set("n", "N", "Nzzzv", opt) -- Keep cursor in the middle when searching
+vim.keymap.set("n", "n", "nzzzv", opt)            -- Keep cursor in the middle when searching
+vim.keymap.set("n", "N", "Nzzzv", opt)            -- Keep cursor in the middle when searching
 
-vim.keymap.set("v", "<", "<gv", opt) -- Indent selected lines
-vim.keymap.set("v", ">", ">gv", opt) -- Indent selected lines
+vim.keymap.set("v", "<", "<gv", opt)              -- Indent selected lines
+vim.keymap.set("v", ">", ">gv", opt)              -- Indent selected lines
 
-vim.keymap.set("n", "<leader>y", "\"+y", opt) -- Copy to system clipboard
-vim.keymap.set("v", "<leader>y", "\"+y", opt) -- Copy to system clipboard
-vim.keymap.set("n", "<leader>Y", "\"+Y", opt) -- Copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "copy to system clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "copy to system clipboard" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "copy to system clipboard" })
 
-vim.keymap.set("n", "<leader>d", "\"_d", opt) -- Delete without yanking
-vim.keymap.set("v", "<leader>d", "\"_d", opt) -- Delete without yanking
-vim.keymap.set("n", "<leader>D", "\"_D", opt) -- Delete without yanking
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete without yanking" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete without yanking" })
+vim.keymap.set("n", "<leader>D", "\"_D", { desc = "Delete without yanking" })
 
-vim.keymap.set("i", "jj", "<Esc>", opt) -- Exit insert mode
+vim.keymap.set("i", "jj", "<Esc>", opt)        -- Exit insert mode
 vim.keymap.set("n", "<C-c>", ":nohl<CR>", opt) -- Clear search highlights
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opt) -- Format code
-vim.keymap.set("n", "x", '"_x', opt) -- Delete character without yanking
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format code" })
+vim.keymap.set("n", "x", '"_x', opt)                                                                                      -- Delete character without yanking
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opt) -- Search and replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace" }) -- Search and replace
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank({
-        })
-    end,
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+    })
+  end,
 })
 
 -- file tabs
-vim.keymap.set("n", "<leader>t", ":tabnew<CR>", opt) -- Open new tab
-vim.keymap.set("n", "<leader>x", ":tabclose<CR>", opt) -- Close tab
-vim.keymap.set("n", "<leader>l", ":tabnext<CR>", opt) -- Go to next tab
+vim.keymap.set("n", "<leader>t", ":tabnew<CR>", opt)      -- Open new tab
+vim.keymap.set("n", "<leader>x", ":tabclose<CR>", opt)    -- Close tab
+vim.keymap.set("n", "<leader>l", ":tabnext<CR>", opt)     -- Go to next tab
 vim.keymap.set("n", "<leader>h", ":tabprevious<CR>", opt) -- Go to previous tab
 
 -- Copy filepath to system clipboard
