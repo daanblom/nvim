390ef8fe76952628dcc5cdc9edd2508a1ce6c3a0
daan <contact@daanblom.com>
2025-07-27 13:50:15 +0200

    replaced telescope with fzflua

---
+++ lua/plugins/fzflua.lua
@@ -0,0 +1,21 @@
+return {
+   {
+    "ibhagwan/fzf-lua",
+    opts = {
+      oldfiles = {
+        include_current_session = true,
+      },
+      previewers = {
+        builtin = {
+          syntax_limit_b = 1024 * 100, -- 100KB
+        },
+      },
+      grep = {
+        rg_glob = true, -- enable glob parsing
+        glob_flag = "--iglob", -- case insensitive globs
+        glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
+      },
+    },
+    vim.keymap.set("n", "e", "<cmd>FzfLua files<CR>", { desc = "File Explorer" })
+  },
+}
