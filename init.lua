a99fc57132fa760981621c4077054c0741637dbc
daan <contact@daanblom.com>
2025-07-27 13:52:07 +0200

    set cursorline

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 require("core")
 require("lazySetup")
-vim.cmd("colorscheme db")
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.guicursor = "n-v-c:block"
 vim.opt_local.conceallevel = 2
 vim.opt.spelllang = { "en", "nl" }
 vim.cmd("set spellfile=~/.vim/spell/en.utf-8.add")
 vim.cmd("set spell!")
+vim.cmd("set cursorline")
