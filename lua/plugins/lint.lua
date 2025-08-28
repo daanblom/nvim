b7a2b0000d994ec95da80a511781c90f2031d2a2
Daan Blom <contact@daanblom.com>
2025-08-28 22:22:47 +0200

    Added markdown linter

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,3 +1,24 @@
 return {
-  "mfussenegger/nvim-lint"
+  "mfussenegger/nvim-lint",
+  config = function()
+    local lint = require("lint")
+
+    -- Copy the default args and append the disable flag
+    local md_args = vim.list_extend(
+      lint.linters.markdownlint.args or { "--stdin" },
+      { "--disable", "MD013" }
+    )
+
+    lint.linters.markdownlint.args = md_args
+
+    lint.linters_by_ft = {
+      markdown = { "markdownlint" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end
 }
