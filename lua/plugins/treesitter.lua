9b9f8fa8ab79dab8e84968511fadaaace207eebe
Daan Blom <contact@daanblom.com>
2026-05-05 13:10:17 +0200

    Updated for neovim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,77 +1,44 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		config = function()
-			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
+  {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local nts = require("nvim-treesitter")
 
-			-- configure treesitter
-			treesitter.setup({ -- enable syntax highlighting
-				highlight = {
-					enable = true,
-				},
-				-- enable indentation
-				indent = { enable = true },
+		nts.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
 
-				-- ensure these languages parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"go",
-					"yaml",
-					"html",
-					"css",
-					"python",
-					"http",
-					"markdown",
-					"markdown_inline",
-					"bash",
-					"lua",
-					"vim",
-					"dockerfile",
-					"gitignore",
-					"query",
-					"vimdoc",
-					"c",
-					"java",
-					"rust",
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-					},
-				},
-				additional_vim_regex_highlighting = false,
-			})
-		end,
-	},
-	-- NOTE: js,ts,jsx,tsx Auto Close Tags
-	{
-		"windwp/nvim-ts-autotag",
-		enabled = true,
-		ft = { "html", "xml", "javascript", "typescript", "javascriptreact" },
-		config = function()
-			-- Independent nvim-ts-autotag setup
-			require("nvim-ts-autotag").setup({
-				opts = {
-					enable_close = true, -- Auto-close tags
-					enable_rename = true, -- Auto-rename pairs
-					enable_close_on_slash = false, -- Disable auto-close on trailing `</`
-				},
-				per_filetype = {
-					["html"] = {
-						enable_close = true, -- Disable auto-closing for HTML
-					},
-					["typescriptreact"] = {
-						enable_close = true, -- Explicitly enable auto-closing (optional, defaults to `true`)
-					},
-				},
-			})
-		end,
-	},
+		nts.install({
+			"json",
+			"javascript",
+			"go",
+			"yaml",
+			"html",
+			"css",
+			"python",
+			"http",
+			"markdown",
+			"markdown_inline",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"java",
+			"rust",
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+	end,
+}
 }
